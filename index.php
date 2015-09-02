1c739599b68ebcd2076b82d45422a339aa62b304
Michel Van der Meiren <michelvdmeiren@gmail.com>
2015-09-02 15:13:39 +0200

    some graphic experiments

--- index.php
+++ index.php
@@ -45,6 +45,8 @@ call_user_func(function(){
 
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
+<figure><img src="<?php echo ROOT; ?>/inc/gold_coins.jpg"></figure>
+
 <?php echo 'Rendered in ', round( microtime(true)-START_TIME , 4 ), ' sec.'; ?>
 
 <nav>
